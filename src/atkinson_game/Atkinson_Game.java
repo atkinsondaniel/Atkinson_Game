363349f2fdbb5db1b2c53a60220ee7b52411804e
Daniel Atkinson <Daniel Atkinson@DESKTOP-8PTVDJI.attlocal.net>
2017-03-06 22:31:46 -0600

    lozl

--- src/atkinson_game/Atkinson_Game.java
+++ src/atkinson_game/Atkinson_Game.java
@@ -18,8 +18,6 @@ import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
-import javafx.scene.control.Button;
-import javafx.scene.layout.StackPane;
 import javafx.scene.image.Image;
 import javafx.scene.input.KeyEvent;
 import javafx.scene.input.MouseEvent;
